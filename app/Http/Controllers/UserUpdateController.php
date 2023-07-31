b91603cac1c66a7857299c5d1fbfde0705381368
-Haru- <52265509+KenPrz@users.noreply.github.com>
2023-07-31 23:01:58 +0800

    readme update

--- app/Http/Controllers/UserUpdateController.php
+++ app/Http/Controllers/UserUpdateController.php
@@ -12,12 +12,12 @@ class UserUpdateController extends Controller
         $request->validate([
             'image' => 'required|image|mimes:jpeg,png,jpg|max:2048',
         ]);
-    
+
         if ($request->hasFile('image')) {
             $image = $request->file('image');
             $imageName = time() . '.' . $image->getClientOriginalExtension();
             $image->storeAs('public/images', $imageName); // Store the image in the storage/app/public/images directory
-    
+
             // Delete the old profile picture if it exists
             if (auth()->user()->profile_image) {
                 Storage::delete('public/' . auth()->user()->profile_image);
@@ -26,8 +26,8 @@ class UserUpdateController extends Controller
             // Update the user's profile with the new image
             auth()->user()->update(['profile_image' => 'images/' . $imageName]);
         }
-    
+
         return redirect()->back()->with('success', 'Image uploaded successfully!');
     }
-    
+
 }
