d26712a2bd2cc45ef2434d102ed3139d95a29a80
Jazel29 <zakuzikutaazkie@gmail.com>
2023-09-05 22:20:37 +0800

    fixing some alignment

--- resources/views/components/rooms_component.blade.php
+++ resources/views/components/rooms_component.blade.php
@@ -27,18 +27,21 @@
 <div class="m-8 mt-2 p-2 grid grid-rows-2 grid-flow-col gap-4 auto-cols-auto rounded-md">
     <div class="flex flex-col text-center">
         <div class="living-room-card rounded-lg bg-white p-8 shadow-md">
-            <div class="living-room-settings flex justify-between items-center">
-                <div class="left-section">
+            <div class="living-room-settings flex">
+                <div class="left-section mx-3">
                     <div id="living_room">
                         <img src="{{ asset('img-assets/vectors/living_room.svg') }}" alt="living_room">
                     </div>
-                    <h1>Living Room</h1>
+                    <h1 class="mt-4 text-xl font-bold">Living Room</h1>
+                    <p class="mt-1 text-sm">Appliance(s)</p>
                 </div>
-                <div class="right-section ml-4">
-                    <label class="relative inline-flex items-center cursor-pointer">
-                        <input type="checkbox" value="" class="sr-only peer" checked>
-                        <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 dark:bg-gray-700 peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                    </label>
+                <div class="right-section ml-4 flex-grow mr-2">
+                    <div class=" flex justify-end">
+                        <label class="relative inline-flex cursor-pointer">
+                            <input type="checkbox" value="" class="sr-only peer" checked>
+                            <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 dark:bg-gray-700 peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                        </label>
+                    </div>
                     <div class="flex flex-col items-start">
                         <div class="mt-3">
                             <h1 class="text-sm">Temperature</h1>
@@ -55,46 +58,21 @@
     </div>
     <div class="flex flex-col text-center">
         <div class="living-room-card rounded-lg bg-white p-8 shadow-md">
-            <div class="living-room-settings flex justify-between items-center">
-                <div class="left-section">
+            <div class="living-room-settings flex">
+                <div class="left-section mx-7">
                     <div id="bedroom">
                         <img src="{{ asset('img-assets/vectors/bedroom.svg') }}" alt="bedroom">
                     </div>
-                    <h1>Bedroom</h1>
+                    <h1 class="mt-4 text-xl font-bold">Bedroom</h1>
+                    <p class="mt-1 text-sm">Appliance(s)</p>
                 </div>
-                <div class="right-section ml-4">
-                    <label class="relative inline-flex items-center cursor-pointer">
-                        <input type="checkbox" value="" class="sr-only peer" checked>
-                        <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 dark:bg-gray-700 peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                    </label>
-                    <div class="flex flex-col items-start">
-                        <div class="mt-3">
-                            <h1 class="text-sm">Temperature</h1>
-                        </div>
-                        <div class="flex w-full border border-gray-600 mt-1 rounded-full h-5"></div>                      
-                        <div class="mt-1">
-                            <p class="text-sm">Humidity</p>
-                        </div>
-                        <div class="flex w-full border border-gray-600 rounded-full mt-1 h-5"></div>  
+                <div class="right-section ml-4 flex-grow mr-2">
+                    <div class=" flex justify-end">
+                        <label class="relative inline-flex cursor-pointer">
+                            <input type="checkbox" value="" class="sr-only peer" checked>
+                            <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 dark:bg-gray-700 peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                        </label>
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="flex flex-col text-center">
-        <div class="living-room-card rounded-lg bg-white p-8 shadow-md">
-            <div class="living-room-settings flex justify-between items-center">
-                <div class="left-section">
-                    <div id="dining_kitchen">
-                        <img src="{{ asset('img-assets/vectors/dining_kitchen.svg') }}" alt="dining_kitchen">
-                    </div>
-                    <h1>Dining & Kitchen</h1>
-                </div>
-                <div class="right-section ml-4"> 
-                    <label class="relative inline-flex items-center cursor-pointer">
-                        <input type="checkbox" value="" class="sr-only peer" checked>
-                        <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 dark:bg-gray-700 peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                    </label>
                     <div class="flex flex-col items-start">
                         <div class="mt-3">
                             <h1 class="text-sm">Temperature</h1>
@@ -111,14 +89,52 @@
     </div>
     <div class="flex flex-col text-center">
         <div class="living-room-card rounded-lg bg-white p-8 shadow-md">
-            <div class="living-room-settings flex justify-between items-center">
-                <div class="left-section">
+            <div class="living-room-settings flex">
+                <div class="left-section mx-7">
+                    <div id="dining_kitchen">
+                        <img src="{{ asset('img-assets/vectors/dining_kitchen.svg') }}" alt="dining_kitchen">
+                    </div>
+                    <h1 class="mt-4 text-xl font-bold">Dining & Kitchen</h1>
+                    <p class="mt-1 text-sm">Appliance(s)</p>
+                </div>
+                <div class="right-section ml-4 flex-grow mr-2">
+                    <div class=" flex justify-end">
+                        <label class="relative inline-flex cursor-pointer">
+                            <input type="checkbox" value="" class="sr-only peer" checked>
+                            <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 dark:bg-gray-700 peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                        </label>
+                    </div>
+                    <div class="flex flex-col items-start">
+                        <div class="mt-3">
+                            <h1 class="text-sm">Temperature</h1>
+                        </div>
+                        <div class="flex w-full border border-gray-600 mt-1 rounded-full h-5"></div>                      
+                        <div class="mt-1"> 
+                            <p class="text-sm">Humidity</p>
+                        </div>
+                        <div class="flex w-full border border-gray-600 rounded-full mt-1 h-5"></div>  
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-col text-center">
+        <div class="living-room-card rounded-lg bg-white p-8 shadow-md">
+            <div class="living-room-settings flex">
+                <div class="left-section mx-7">
                     <div id="bathroom">
                         <img src="{{ asset('img-assets/vectors/bathroom.svg') }}" alt="bathroom">
                     </div>
-                    <h1>Bathroom</h1>
+                    <h1 class="mt-2 text-xl font-bold">Bathroom</h1>
+                    <p class="mt-1 text-sm">Appliance(s)</p>
                 </div>
-                <div class="right-section">
+                <div class="right-section ml-4 flex-grow mr-2">
+                    <div class=" flex justify-end">
+                        <label class="relative inline-flex cursor-pointer">
+                            <input type="checkbox" value="" class="sr-only peer" checked>
+                            <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 dark:bg-gray-700 peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                        </label>
+                    </div>
                 </div>
             </div>
         </div>
